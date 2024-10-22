e379d3e1f514e20d42ecb1e23527b26693c89acb
nguyenminhlinh <linhh.nguyenminh@gmail.com>
2024-10-22 17:19:49 +0700

    Feat: Add func scan vulnerability

---
+++ pkg/collector/vuln/vuln.go
@@ -0,0 +1,90 @@
+package vuln
+
+import (
+	"context"
+	"fmt"
+	data "recon/pkg/data/type"
+	"sync"
+	"time"
+
+	nuclei "github.com/projectdiscovery/nuclei/v3/lib"
+	"github.com/projectdiscovery/nuclei/v3/pkg/output"
+)
+
+func ScanVulnerability(listScanVuln map[string]bool, ctx context.Context, subDomainChan chan string, infoAllVulnerability map[string][]data.InforVulnerability, typeScan int) {
+	var timeOut time.Duration
+	var mu sync.Mutex
+	if typeScan == 1 {
+		timeOut = 5 * time.Minute // Set the timeout by configuring the time for the context
+	} else if typeScan == 2 {
+		timeOut = 10 * time.Minute // Set the timeout by configuring the time for the context
+	} else if typeScan == 3 {
+		timeOut = 20 * time.Minute // Set the timeout by configuring the time for the context
+	}
+	ctxTimeout, cancelTimeout := context.WithTimeout(ctx, timeOut)
+	defer cancelTimeout()
+	// create nuclei engine with options
+	// setup sizedWaitgroup to handle concurrency
+	// here we are using sizedWaitgroup to limit concurrency to 1
+	// but can be anything in general
+	ne, err := nuclei.NewThreadSafeNucleiEngineCtx(ctxTimeout)
+	if err != nil {
+		fmt.Println("NewThreadSafeNucleiEngineCtx", err)
+		return
+	}
+
+	ne.GlobalLoadAllTemplates()
+	var wg sync.WaitGroup
+
+	for i := 0; i < 10; i++ {
+		for subDomain := range subDomainChan {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				err = ne.ExecuteNucleiWithOptsCtx(ctx, []string{subDomain},
+					nuclei.WithTemplateFilters(nuclei.TemplateFilters{}),
+				)
+				if err != nil {
+					fmt.Println("ExecuteNucleiWithOptsCtx", err)
+					return
+				}
+				listScanVuln[subDomain] = true
+			}()
+		}
+	}
+
+	ne.GlobalResultCallback(func(event *output.ResultEvent) {
+		var inforVulnerability data.InforVulnerability
+		inforVulnerability.NameSubDomain = event.Host
+		inforVulnerability.TemplateID = event.TemplateID
+		inforVulnerability.ExtractorName = event.ExtractorName
+		inforVulnerability.Matched = event.Matched
+		inforVulnerability.MatcherName = event.MatcherName
+		inforVulnerability.Type = event.Type
+		inforVulnerability.TemplateName = event.Info.Name
+		inforVulnerability.Classification = fmt.Sprintf("%v", event.Info.Classification)
+
+		if len(event.Info.Description) > 0 {
+			inforVulnerability.TemplateDescription = event.Info.Description
+		}
+
+		if len(event.Info.SeverityHolder.Severity.String()) > 0 {
+			inforVulnerability.Severity = event.Info.SeverityHolder.Severity.String()
+		}
+
+		if len(event.ExtractedResults) > 0 {
+			inforVulnerability.ExtractedResults = event.ExtractedResults
+		}
+
+		mu.Lock() // Lock map before accessing it
+		infoVulnerabilitySlice := infoAllVulnerability[event.Host]
+		infoVulnerabilitySlice = append(infoVulnerabilitySlice, inforVulnerability)
+		infoAllVulnerability[event.Host] = infoVulnerabilitySlice
+		mu.Unlock()
+	})
+
+	//wait for all scans to finish
+	wg.Wait()
+	defer ne.Close()
+
+}
--- pkg/data/type/data.go
+++ pkg/data/type/data.go
@@ -2,29 +2,44 @@ package data
 
 import wappalyzer "github.com/projectdiscovery/wappalyzergo"
 
+type InforVulnerability struct {
+	NameSubDomain       string   `json:"namesubdomain"`
+	Type                string   `json:"type"`
+	TemplateID          string   `json:"templateid"`
+	Severity            string   `json:"severity"`
+	ExtractorName       string   `json:"extractorname"`
+	MatcherName         string   `json:"matchername"`
+	Matched             string   `json:"matched"`
+	TemplateName        string   `json:"templatename"`
+	TemplateDescription string   `json:"templatedescription"`
+	ExtractedResults    []string `json:"extractedresults"`
+	Classification      string   `json:"classification"`
+}
+
 type InfoWeb struct {
 	TechnologyDetails map[string]wappalyzer.AppInfo `json:"technologydetails"`
 	Link              []string                      `json:"link"`
-	Vulnerability     string                        `json:"vulnerability"`
 	Status            string                        `json:"status"`
 	Title             string                        `json:"title"`
 }
 
 type InfoSubDomain struct {
-	NameSubDomain  string             `json:"namesubdomain"`
-	Ips            []string           `json:"ips"`
-	PortAndService map[string]string  `json:"portsandservice"`
-	Os             []string           `json:"os"`
-	HttpOrHttps    map[string]InfoWeb `json:"httporhttps"`
-	CName          []string           `json:"cname"`
+	NameSubDomain     string             `json:"namesubdomain"`
+	Ips               []string           `json:"ips"`
+	PortAndService    map[string]string  `json:"portsandservice"`
+	Os                []string           `json:"os"`
+	Web               map[string]InfoWeb `json:"httporhttps"`
+	CName             []string           `json:"cname"`
+	FlagVulnerability bool               `json:"flagvulnerability"`
 }
 
 type InfoDomain struct {
-	MXRecords  []string                 `json:"mxrecords"`
-	NSRecords  []string                 `json:"nsrecords"`
-	SOARecords []string                 `json:"soarecords"`
-	TXTRecords []string                 `json:"txtrecords"`
-	SubDomain  map[string]InfoSubDomain `json:"subdomain"`
+	MXRecords     []string                        `json:"mxrecords"`
+	NSRecords     []string                        `json:"nsrecords"`
+	SOARecords    []string                        `json:"soarecords"`
+	TXTRecords    []string                        `json:"txtrecords"`
+	Vulnerability map[string][]InforVulnerability `json:"vulnerability"`
+	SubDomain     map[string]InfoSubDomain        `json:"subdomain"`
 }
 
 var ListDomain = make(map[string]InfoDomain)
